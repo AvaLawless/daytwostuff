0e3225945cc2f27af782af250b616b0a5c8b3249
Ava Lawless <psz6ah@virginia.edu>
2026-01-15 20:00:24 +0000

    Add python file, import dataset, update requirements

---
+++ pythonfile.py
@@ -0,0 +1,22 @@
+# What is your terminal display "path"? (type/paste as text into the .py file)?
+# When I first opened the terminal, the terminal display "path" is /workspaces/daytwostuff
+# 
+# Should you include the environment in your repo or not?
+# No, you should not include a virtual environment in your repo. Virtual environments are individual the person using it, so it will not be useful to others.
+#
+# Now what is your terminal display "path"? Is it different?
+# The terminal display "path" is (.venv) /workspaces/daytwostuff. It's different because it now shows that I am using a virtual environment
+#
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.read_csv('/workspaces/daytwostuff/google-tesla.csv')
+
+# Extension management
+# Find the extension in the extension menu. What do you notice about the extension menu?
+# The extension menu shows extensions that I can install to my environment. It shows the option to install to the codespaces. 
+#
+# Review the capabilities, what are three useful elements of Data Wrangler?
+# Data wrangler allows you to clean and visualize large datasets, integrates with pandas, and allows you to export code for data transformations.
